abd23f161e88d2c0cde2083135f68e766aee7dc2
NMakarevich <nikoladze529@mail.ru>
2025-09-02 01:04:08 +0300

    fix: fix middleware

--- src/middlewares/request-api.middleware.ts
+++ src/middlewares/request-api.middleware.ts
@@ -1,4 +1,4 @@
 export function requestApi(req: any, res: any, next: () => void) {
-  req.url = req.url.split('api')[1];
+  req.url = req.url.startsWith('/api') ? req.url.split('api')[1] : req.url;
   next();
 }
